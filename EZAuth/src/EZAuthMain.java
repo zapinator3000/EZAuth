cffcec8e113fdbfaaa20a70f6bd3d2eddaea1035
zpain <zpain@Zackery-Windows>
2020-05-30 01:08:56 -0400

    AccessManager

--- EZAuth/src/EZAuthMain.java
+++ EZAuth/src/EZAuthMain.java
@@ -1,10 +1,36 @@
 
-public class EZAuthMain {
-public static void main(String[] args) {
-	new EZAuthMain();
-}
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public EZAuthMain() {
-	
-}
+import javax.swing.Timer;
+
+import com.macasaet.fernet.Key;
+import com.macasaet.fernet.StringValidator;
+import com.macasaet.fernet.Token;
+import com.macasaet.fernet.Validator;
+
+public class EZAuthMain implements ActionListener {
+	private Timer timer;
+	private AccessManager accessManager;
+	Key key;
+
+	public static void main(String[] args) {
+		new EZAuthMain();
+	}
+
+	public EZAuthMain() {
+		this.timer = new Timer(10, this);
+		this.accessManager = new AccessManager();
+		key = this.accessManager.generateKey();
+		this.timer.start();
+		while(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println(
+				"Ticks: " + this.accessManager.getServerTicks() + " : " + this.accessManager.checkGameValidity(key));
+		this.accessManager.update();
+	}
 }
